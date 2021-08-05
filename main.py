85ade5c7270c31b0821c3dbe8abbb5f7d2ea7b22
HiddenSquid321 <85686367+HiddenSquid321@users.noreply.github.com>
2021-08-05 13:05:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,33 +3,32 @@ import time
 
 start_time = time.time()
 
-if __name__=='__main__':
-	grid = [[8,0,0,0,0,0,0,0,0], 
-	 	[0,0,3,6,0,0,0,0,0],
-	 	[0,7,0,0,9,0,2,0,0],
-	 	[0,5,0,0,0,7,0,0,0],
-	 	[0,0,0,0,4,5,7,0,0],
-	 	[0,0,0,1,0,0,0,3,0],
-	 	[0,0,1,0,0,0,0,6,8],
-	 	[0,0,8,5,0,0,0,1,0],
-	 	[0,9,0,0,0,0,4,0,0]]
+grid = [[0,0,0,0,0,1,2,0,0], 
+	 	[0,0,0,0,0,0,3,4,0],
+	 	[0,0,0,0,0,0,0,5,6],
+	 	[0,0,0,0,0,0,0,0,7],
+	 	[0,0,0,0,0,0,0,0,0],
+	 	[1,0,0,0,0,0,0,0,0],
+	 	[7,2,0,0,0,0,0,0,0],
+	 	[0,4,8,0,0,0,0,0,0],
+	 	[0,0,6,3,0,0,0,0,0]]
 
 print (np.matrix(grid))
 
-def find_empty_box(puzzle): 
+def find_empty_box(sudoku): 
 	for x in range(9):
 		for y in range(9):
-			if puzzle[x][y] == 0:
+			if sudoku[x][y] == 0:
 				return x, y 
 	
 	return None, None
 
-def Answer_Valid(puzzle, guess, row, col): 
-	row_values = puzzle[row]    
+def Answer_Valid(sudoku, guess, row, col): 
+	row_values = sudoku[row]    
 	if guess in row_values:
 		return False
 
-	column_values = [puzzle[i][col]for i in range(9)]
+	column_values = [sudoku[i][col]for i in range(9)]
 	if guess in column_values:
 		return False
 
@@ -38,25 +37,25 @@ def Answer_Valid(puzzle, guess, row, col):
 
 	for x in range(row_start, row_start + 3):
 		for y in range(col_start, col_start + 3):
-			if puzzle[x][y] == guess:
+			if sudoku[x][y] == guess:
 				return False
 
 		return True
 
-def Solver(puzzle):
-	row, col = find_empty_box(puzzle)
+def Solver(sudoku):
+	row, col = find_empty_box(sudoku)
 
 	if row is None:
 		return True
 
 	for guess in range(1,10):
-		if Answer_Valid(puzzle, guess, row, col):
-			puzzle[row][col] = guess
+		if Answer_Valid(sudoku, guess, row, col):
+			sudoku[row][col] = guess
 
-			if Solver(puzzle):
+			if Solver(sudoku):
 				return True
 
-		puzzle[row][col] = 0
+		sudoku[row][col] = 0
 
 	return False
 
