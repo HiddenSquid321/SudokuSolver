b9603d4d2e084078fc76865231c4c1edac2410da
HiddenSquid321 <85686367+HiddenSquid321@users.noreply.github.com>
2021-08-14 18:43:04 +0100

    Update Commented.py

--- Commented.py
+++ Commented.py
@@ -38,8 +38,8 @@ def Answer_Valid(sudoku, guess, row, col):
 	col_start = (col // 3) * 3
 	
 	# for loops and if statements to check if the box is valid
-	for x in range(row_start, row_start + 3): 
-		for y in range(col_start, col_start + 3):
+	for y in range(row_start, row_start + 3): 
+		for x in range(col_start, col_start + 3):
 			if sudoku[y][x] == guess:
 				return False
 
